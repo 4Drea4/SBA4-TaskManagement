b3dd107f79f7e5fdbf72ac0677e52a4b675d2e66
4drea4 <bakerand@outlook.com>
2025-10-29 12:34:50 -0500

    looking up whats going wrong and right with my code. I did not enter the array correctly

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-
+//declarations
 let taskTitle =document.getElementById("task-name");//title of task
 let progress = document.getElementById("progress"); //progress drop-down
 let addTask= document.getElementById("submitTask");//button
@@ -7,26 +7,52 @@ let deadline = document.getElementById("task-due-date");
 let category = document.getElementById("category-options");
 const myTask = document.body;
 
-//The array of objects 
+//The array of objects for the form
 const task = [
-    {title:taskTitle.value  },
-    {status: ["not-started" ,"in-progress", "finished" ]},
-    {category:["home" ,"in career", "personal" , ]},
-    {deadline: task-due-date},
- tasks.push(task)
+    {title: "", 
+    progress:"",
+    category:"", 
+    deadline:""},    
+
+    
 ];
 
-progress.value
+addTask.addEventListener("click", function )() {
+    let taskTitle = taskTitle.value;
+   
+    if (taskTitle === "") {
+      alert("Please enter a task!");
+      return;
+    }
+    tasks.push(task) //pushing new form submissions of objects to the array of objects
 
+    //let progress = document.createElement("li");
+    //listItem.innerText = taskText;
+   
+    //taskList.appendChild(listItem);
+   // taskInput.value = ""; // Clear the input field
+  //});
+//progress.value //I know I have to capture the value of inputs just unsure of if this should go into a sep. function.
+
+//function to create a new task item.
+function createTask() {
+    let task = document.getElementById('task-name').value;
+    if (!(typeof task === 'undefined') && task!=null && take.trim()!='') {  
+        createTask.push(task);
+    }
+    console.log(createTask);
+    document.getElementById('task-name').value = '';
+}
 //Create an onclick for the sumbit button here
-
+//document.submitButton("button");
 
 
 
 //Create and append select list
-//let submitTask = document.submitButton("button");
-//selectTask.id = "mySelect";
-//myParent.appendChild(selectList);
+
+
+//selectTask.id ;
+//task.appendChild(createTask);
 
 //Create and append the options
 //for (let i = 0; i < array.length; i++) {
@@ -34,4 +60,4 @@ progress.value
    // option.value = array[i];
    // option.text = array[i];
   //  selectList.appendChild(option)
-//get the text that the user enters here
+
