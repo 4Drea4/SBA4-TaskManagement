35a80ce7ff6d00889bf0a6b140c30a9637c2c407
4drea4 <bakerand@outlook.com>
2025-10-29 12:47:47 -0500

    correcting my comments

--- index.js
+++ index.js
@@ -2,20 +2,22 @@
 //declarations
 let taskTitle =document.getElementById("task-name");//title of task
 let progress = document.getElementById("progress"); //progress drop-down
+let deadline = document.getElementById("task-due-date");//due-date calendar
+let category = document.getElementById("category-options");//category drop-down
 let addTask= document.getElementById("submitTask");//button
-let deadline = document.getElementById("task-due-date");
-let category = document.getElementById("category-options");
-const myTask = document.body;
+
+//const myTask = document.body;
 
 //The array of objects for the form
-const task = [
-    {title: "", 
-    progress:"",
-    category:"", 
-    deadline:""},    
-
+const task = {
+    title: taskTitle.value,
+    progress: progress.value,
+    category: category.value,
+    deadline:deadline.value,
     
-];
+}
+
+//Create an onclick for the sumbit button here
 
 addTask.addEventListener("click", function )() {
     let taskTitle = taskTitle.value;
@@ -26,13 +28,7 @@ addTask.addEventListener("click", function )() {
     }
     tasks.push(task) //pushing new form submissions of objects to the array of objects
 
-    //let progress = document.createElement("li");
-    //listItem.innerText = taskText;
    
-    //taskList.appendChild(listItem);
-   // taskInput.value = ""; // Clear the input field
-  //});
-//progress.value //I know I have to capture the value of inputs just unsure of if this should go into a sep. function.
 
 //function to create a new task item.
 function createTask() {
@@ -43,21 +39,11 @@ function createTask() {
     console.log(createTask);
     document.getElementById('task-name').value = '';
 }
-//Create an onclick for the sumbit button here
-//document.submitButton("button");
 
 
 
-//Create and append select list
-
-
-//selectTask.id ;
-//task.appendChild(createTask);
-
-//Create and append the options
-//for (let i = 0; i < array.length; i++) {
-   // var option = document.createElement("select");
-   // option.value = array[i];
-   // option.text = array[i];
-  //  selectList.appendChild(option)
+for (let i=0; i<4; i++ ) {
+    const newList = document.createElement('li');
+    newList.innerText
+}
 
