1d015a81060322e8fa6204420789c25e2370293a
4drea4 <bakerand@outlook.com>
2025-10-29 10:29:59 -0500

    set up project files, finished task 1, created html elements and form, began creating the object for form data to be saved into, working on getting things saved into an array

---
+++ index.js
@@ -0,0 +1,35 @@
+
+
+let taskTitle =document.getElementById("task-name");//title of task
+let progress = document.getElementById("progress"); //progress drop-down
+let addTask= document.getElementById("submitTask");//button
+let deadline = document.getElementById("task-due-date");
+let category = document.getElementById("category-options");
+const myTask = document.body;
+//The array of objects 
+const task = [
+    {title:taskTitle  }
+    {status: progress-state[not-started ,in-progress, finished ]}
+    {category: category-options[home , , ]}
+    {deadline: task-due-date}
+
+]
+
+
+
+//Create an onclick for the sumbit button here
+
+
+
+
+//Create and append select list
+let submitTask = document.submitButton("button");
+selectTask.id = "mySelect";
+myParent.appendChild(selectList);
+
+//Create and append the options
+for (let i = 0; i < array.length; i++) {
+    var option = document.createElement("select");
+    option.value = array[i];
+    option.text = array[i];
+    selectList.appendChild(option)
