f6db9e6eac20890cdf249ea5b7d0a820cc498c5c
4drea4 <bakerand@outlook.com>
2025-10-29 10:40:39 -0500

    working on storing the form values into an array.

--- index.js
+++ index.js
@@ -6,16 +6,17 @@ let addTask= document.getElementById("submitTask");//button
 let deadline = document.getElementById("task-due-date");
 let category = document.getElementById("category-options");
 const myTask = document.body;
+
 //The array of objects 
 const task = [
-    {title:taskTitle  }
-    {status: progress-state[not-started ,in-progress, finished ]}
-    {category: category-options[home , , ]}
-    {deadline: task-due-date}
+    {title:taskTitle.value  },
+    {status: ["not-started" ,"in-progress", "finished" ]},
+    {category:["home" ,"in career", "personal" , ]},
+    {deadline: task-due-date},
+ tasks.push(task)
+];
 
-]
-
-
+progress.value
 
 //Create an onclick for the sumbit button here
 
@@ -23,13 +24,14 @@ const task = [
 
 
 //Create and append select list
-let submitTask = document.submitButton("button");
-selectTask.id = "mySelect";
-myParent.appendChild(selectList);
+//let submitTask = document.submitButton("button");
+//selectTask.id = "mySelect";
+//myParent.appendChild(selectList);
 
 //Create and append the options
-for (let i = 0; i < array.length; i++) {
-    var option = document.createElement("select");
-    option.value = array[i];
-    option.text = array[i];
-    selectList.appendChild(option)
+//for (let i = 0; i < array.length; i++) {
+   // var option = document.createElement("select");
+   // option.value = array[i];
+   // option.text = array[i];
+  //  selectList.appendChild(option)
+//get the text that the user enters here
